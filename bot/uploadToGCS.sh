4c7e1c2fef552f546d25aed31ecb34257bc0b111
mattrankin10 <mattprankin@gmail.com>
2020-03-27 11:39:42 +0000

    fix bash script

--- bot/uploadToGCS.sh
+++ bot/uploadToGCS.sh
@@ -8,6 +8,6 @@ if [[ -n "$bucketname" ]]; then
   gsutil cp test.from gs://$bucketname
   gsutil cp test.to gs://$bucketname
   echo "Files uploaded to bucket"
-fi
 else
     echo "argument error"
+fi
